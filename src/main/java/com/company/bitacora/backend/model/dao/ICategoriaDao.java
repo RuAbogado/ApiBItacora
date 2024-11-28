6a65a31ea0fcc6e1950fad4004a241d0b95cf53b
Nadia05Sa <20233tn237@utez.edu.mx>
2024-11-27 18:09:55 -0600

    ya casi termino

--- src/main/java/com/company/bitacora/backend/model/dao/ICategoriaDao.java
+++
@@ -1,8 +0,0 @@
-package com.company.bitacora.backend.model.dao;
-
-import com.company.bitacora.backend.model.Categoria;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ICategoriaDao extends CrudRepository<Categoria, Long> {
-
-}
