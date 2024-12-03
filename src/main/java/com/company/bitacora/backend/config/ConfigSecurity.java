734948ea1c37b063465826e17ad2001f2831f835
Nadia05Sa <20233tn237@utez.edu.mx>
2024-12-03 14:57:30 -0600

    Cambios

--- src/main/java/com/company/bitacora/backend/config/ConfigSecurity.java
+++ src/main/java/com/company/bitacora/backend/config/ConfigSecurity.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,81 +21,69 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import javax.sql.DataSource;
 import java.util.List;
 
-@Configuration // Indica que esta clase es una configuración de Spring.
+@Configuration
 public class ConfigSecurity {
 
     @Autowired
     @Lazy
-    private JwtReqFilter jwtReqFilter; // Se inyecta un filtro que gestionará la validación de JWT.
+    private JwtReqFilter jwtReqFilter;
 
-    // Bean que define el servicio de detalles de usuario, en este caso se utiliza un JdbcUserDetailsManager
-    // para obtener los usuarios de una base de datos.
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
 
-    // Bean que define la configuración de seguridad HTTP, donde se configuran las reglas de acceso
-    // y autenticación para las rutas de la API.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configure -> configure
-                        // Se configuran las reglas de autorización para las rutas según el método HTTP (GET, POST, PUT, DELETE)
-                        .requestMatchers(HttpMethod.GET, "/v1/alumnos").hasAnyRole("Admin", "Alumno") // Solo Admin y Alumno pueden acceder
-                        .requestMatchers(HttpMethod.POST, "/v1/alumnos").hasAnyRole("Admin", "Alumno") // Solo Admin y Alumno pueden acceder
-                        .requestMatchers(HttpMethod.PUT, "/v1/alumnos/**").hasAnyRole("Admin", "Alumno") // Solo Admin y Alumno pueden acceder
-                        .requestMatchers(HttpMethod.DELETE, "/v1/alumnos/**").hasRole("Admin") // Solo Admin puede eliminar
-                        .requestMatchers(HttpMethod.GET, "/v1/bitacoras").hasAnyRole("Admin", "Empleado") // Admin y Empleado pueden acceder
-                        .requestMatchers(HttpMethod.POST, "/v1/bitacoras").hasRole("Alumno") // Solo Alumno puede crear
-                        .requestMatchers(HttpMethod.PUT, "/v1/bitacoras/**").hasRole("Empleado") // Solo Empleado puede modificar
-                        .requestMatchers(HttpMethod.DELETE, "/v1/bitacoras/**").hasAnyRole("Empleado", "Admin") // Empleado y Admin pueden eliminar
-                        .requestMatchers(HttpMethod.GET, "/v1/equipos").hasAnyRole("Admin", "Empleado") // Admin y Empleado pueden acceder
-                        .requestMatchers(HttpMethod.POST, "/v1/equipos").hasRole("Admin") // Solo Admin puede crear
-                        .requestMatchers(HttpMethod.PUT, "/v1/equipos/**").hasRole("Admin") // Solo Admin puede modificar
-                        .requestMatchers(HttpMethod.DELETE, "/v1/equipos/**").hasRole("Admin") // Solo Admin puede eliminar
-                        .requestMatchers(HttpMethod.GET, "/v1/salones").hasAnyRole("Admin", "Empleado") // Admin y Empleado pueden acceder
-                        .requestMatchers(HttpMethod.POST, "/v1/salones").hasRole("Admin") // Solo Admin puede crear
-                        .requestMatchers(HttpMethod.PUT, "/v1/salones/**").hasRole("Admin") // Solo Admin puede modificar
-                        .requestMatchers(HttpMethod.DELETE, "/v1/salones/**").hasRole("Admin") // Solo Admin puede eliminar
-                        .requestMatchers(HttpMethod.GET, "/v1/tecnicos").hasRole("Admin") // Solo Admin puede acceder
-                        .requestMatchers(HttpMethod.POST, "/v1/tecnicos").hasRole("Admin") // Solo Admin puede crear
-                        .requestMatchers(HttpMethod.PUT, "/v1/tecnicos/**").hasRole("Admin") // Solo Admin puede modificar
-                        .requestMatchers(HttpMethod.DELETE, "/v1/tecnicos/**").hasRole("Admin") // Solo Admin puede eliminar
-                        .requestMatchers("/v1/authenticate").permitAll() // Ruta pública para la autenticación
+                        .requestMatchers(HttpMethod.GET, "/v1/alumnos").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/alumnos").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/v1/alumnos/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/v1/alumnos/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/bitacoras").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/bitacoras").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/v1/bitacoras/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/v1/bitacoras/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/equipos").hasAnyRole("Admin", "Empleado")
+                        .requestMatchers(HttpMethod.POST, "/v1/equipos").hasRole("Admin")
+                        .requestMatchers(HttpMethod.PUT, "/v1/equipos/**").hasRole("Admin")
+                        .requestMatchers(HttpMethod.DELETE, "/v1/equipos/**").hasRole("Admin")
+                        .requestMatchers(HttpMethod.GET, "/v1/salones").hasRole("Admin")
+                        .requestMatchers(HttpMethod.POST, "/v1/salones").hasRole("Admin")
+                        .requestMatchers(HttpMethod.PUT, "/v1/salones/**").hasRole("Admin")
+                        .requestMatchers(HttpMethod.DELETE, "/v1/salones/**").hasRole("Admin")
+                        .requestMatchers(HttpMethod.GET, "/v1/tecnicos").hasRole("Admin")
+                        .requestMatchers(HttpMethod.POST, "/v1/tecnicos").hasRole("Admin")
+                        .requestMatchers(HttpMethod.PUT, "/v1/tecnicos/**").hasRole("Admin")
+                        .requestMatchers(HttpMethod.DELETE, "/v1/tecnicos/**").hasRole("Admin")
+                        .requestMatchers("/v1/authenticate").permitAll()
                 )
-                // Configuración de CORS para permitir solicitudes desde un origen específico
                 .cors(cors -> cors.configurationSource(request -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:8080")); // Ajusta el origen según el cliente
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE")); // Métodos permitidos
-                    config.setAllowCredentials(true); // Permite credenciales en las solicitudes CORS
-                    config.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Especificar encabezados permitidos
-                    config.setExposedHeaders(List.of("Authorization")); // Encabezados visibles para el cliente                    return config;
+                    config.setAllowedOrigins(List.of("http://localhost:8080"));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+                    config.setExposedHeaders(List.of("Authorization"));
                     return config;
                 }))
-                // Deshabilitar CSRF (Cross-Site Request Forgery) porque la aplicación probablemente está utilizando JWT
                 .csrf(csrf -> csrf.disable())
-                // Configuración para que la aplicación no mantenga sesiones (stateless), ya que se usa JWT para autenticación
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // Añadir el filtro JWT antes del filtro de autenticación por nombre de usuario y contraseña
                 .addFilterBefore(jwtReqFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build(); // Construir la configuración de seguridad
+        return http.build();
     }
 
-    // Bean que proporciona un AuthenticationManager para la autenticación de usuarios
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    // Bean que define un conjunto de usuarios en memoria para pruebas, sin usar una base de datos.
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        // Definir usuarios con sus credenciales y roles
         UserDetails admin = User.builder()
                 .username("admin")
-                .password("{noop}admin1234") // {noop} se utiliza para contraseñas en texto claro, no recomendadas en producción
+                .password("{noop}admin1234")
                 .roles("Admin")
                 .build();
 
@@ -112,7 +99,6 @@ public class ConfigSecurity {
                 .roles("Alumno")
                 .build();
 
-        // Crear y devolver un manager de usuarios en memoria con los usuarios definidos
         return new InMemoryUserDetailsManager(admin, hugo, edita);
     }
 }
